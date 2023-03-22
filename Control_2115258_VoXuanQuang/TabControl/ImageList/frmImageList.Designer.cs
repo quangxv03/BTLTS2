0cc4db3b2d0f7a84495fbee1148b24b8992e604c
quangxv03 <113871496+quangxv03@users.noreply.github.com>
2023-03-22 23:16:28 +0700

    upload

---
+++ Control_2115258_VoXuanQuang/TabControl/ImageList/frmImageList.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ImageList
+{
+    partial class frmImageList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmImageList));
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("View", 0);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Volume Manager", 1);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("Windows", 2);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "xcancel.png");
+            this.imageList1.Images.SetKeyName(1, "2232688.png");
+            this.imageList1.Images.SetKeyName(2, "2541991.png");
+            this.imageList1.Images.SetKeyName(3, "3075867.png");
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem4,
+            listViewItem5,
+            listViewItem6});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(245, 140);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Demo ImageList";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Control_2115258_VoXuanQuang/TabControl/TabControl/frmTabControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabControl
+{
+    public partial class frmTabControl : Form
+    {
+        public frmTabControl()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTabControl_Load(object sender, EventArgs e)
+        {
+            this.tabControl1.TabPages.Clear();
+            TabPage tabSV = new TabPage();
+            TabPage tabGV = new TabPage();
+            TabPage tabMH = new TabPage();
+            tabSV.Text = "Sinh viên";
+            tabGV.Text = "Giáo viên";
+            tabMH.Text = "Môn học";
+            this.tabControl1.TabPages.Add(tabSV);
+            this.tabControl1.TabPages.Add(tabGV);
+            this.tabControl1.TabPages.Add(tabMH);
+        }
+    }
+}
